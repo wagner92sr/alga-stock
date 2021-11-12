98dd67387dedbd25de9801d12a3deb1a8d79cfbd
Wagner Rodrigues <wagnersr92@gmail.com>
2021-11-11 23:30:53 -0300

    first useState

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent />
+        <TestComponent name="Wagner" />
         <ClassComponent name="Mundo" />
         <a
           className="App-link"
