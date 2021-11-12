8aeb64c5d499ed021274d362130bd8eec8955249
Wagner Rodrigues <wagnersr92@gmail.com>
2021-11-11 21:12:31 -0300

    create TestComponent and move App to the components directory

---
+++ src/components/TestComponent/index.ts
@@ -0,0 +1,7 @@
+export { default } from "./TestComponent";
+/*
+Serve para fazer import no App.tsx sem ter que informar o nome do componente
+quando tem o mesmo nome da pasta
+import TestComponent from './components/TestComponent/TestComponent';
+import TestComponent from './components/TestComponent;
+*/
