265ae0e87c0d0195ea56c0d2197e868d1362e2e9
Wagner Rodrigues <wagnersr92@gmail.com>
2021-11-13 18:02:50 -0300

    criação shared component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Button from '../Button';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
 import Header from '../Header';
 import './App.css';
 
@@ -12,15 +13,16 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
 
-      <div className="Container">
+      <Container>
         <Button
           onClick={() => window.alert('UIIIU')}
           appendIcon={<TestComponent />}
         >
           Alert
         </Button>
-      </div>
+      </Container>
     </div>
+
   );
 }
 
