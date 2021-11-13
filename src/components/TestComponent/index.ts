37e21b8a73e04334327a8644d83fcad8e4f17831
Wagner Rodrigues <wagnersr92@gmail.com>
2021-11-12 22:03:27 -0300

    clear whole application

--- src/components/TestComponent/index.ts
+++
@@ -1,7 +0,0 @@
-export { default } from "./TestComponent";
-/*
-Serve para fazer import no App.tsx sem ter que informar o nome do componente
-quando tem o mesmo nome da pasta
-import TestComponent from './components/TestComponent/TestComponent';
-import TestComponent from './components/TestComponent;
-*/
