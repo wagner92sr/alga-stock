09d860011e3e6ddf4c04d1c2e0666f27505cac44
Wagner Rodrigues <wagnersr92@gmail.com>
2021-11-16 19:41:59 -0300

    clear app + add table component's base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
-import Button from '../../shared/Button';
+import React, { } from 'react';
 import Container from '../../shared/Container';
 import Header from '../Header';
-import Input from '../../shared/input/Input';
 import './App.css';
 
-function TestComponent() {
-  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
-}
+
 
 function App() {
-  const [street, setStreet] = useState('')
 
   return (
     <div className="App">
       <Header title="AlgaStock" />
-
       <Container>
-        <ul>
-          {
-            ['Daniel', 'William', 'Thiago'].map((name, index) => {
-              return <li key={index}>
-                {name}
-              </li>
-            })
-          }
-        </ul>
+
       </Container>
     </div>
 
